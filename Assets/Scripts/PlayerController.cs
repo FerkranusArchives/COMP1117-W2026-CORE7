c85309946c25b85bd6d4b2462adea3e82adbbd7a
Emma Ferkranus <emmaferk@gmail.com>
2026-01-14 13:22:45 -0500

    Added project files, wiped scripts for my own work

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,50 +1,16 @@
 using UnityEngine;
-using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
-    [Header("Initial Player Stats")]
-    // Initial Player Stats
-    [SerializeField] private float initialSpeed = 5;
-    [SerializeField] private int initialHealth = 100;
-
-    // Private variables
-    private PlayerStats stats;
-    private Vector2 moveInput;
-
-    // Components
-    private Rigidbody2D rBody;
-
-    void Awake()
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
     {
-        // Initialize
-        rBody = GetComponent<Rigidbody2D>();
-
-        stats = new PlayerStats(initialSpeed, initialHealth);
+        
     }
 
-    void OnMove(InputValue value)
+    // Update is called once per frame
+    void Update()
     {
-        moveInput = value.Get<Vector2>();
-    }
-
-    void FixedUpdate()
-    {
-        ApplyMovement();
-    }
-
-    private void ApplyMovement()
-    {
-        float velocityX = moveInput.x * stats.MoveSpeed;
-
-        rBody.linearVelocity = new Vector2(velocityX, rBody.linearVelocity.y);
-    }
-
-    public void TakeDamage(int damageAmount)
-    {
-        stats.CurrentHealth -= damageAmount;
-        // stats.CurrentHealth = stats.CurrentHealth - damageAmount;
-
-        Debug.Log("Player took damage");
+        
     }
 }
