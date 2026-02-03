07c356d57ab139545efc2cecd1b78079983d608d
Emma Ferkranus <emmaferk@gmail.com>
2026-02-03 13:01:19 -0500

    Attempted Elite Enemy

--- Assets/Scripts/Enemies/EliteEnemy.cs
+++ Assets/Scripts/Enemies/EliteEnemy.cs
@@ -5,10 +5,18 @@ public class EliteEnemy : Enemy
     protected override void Awake()
     {
         base.Awake();
+        
     }
+
 
     protected override void Update()
     {
         base.Update();
+
+    }
+
+    protected void EliteTransform()
+    {
+        transform.localScale *= 2f;
     }
 }
